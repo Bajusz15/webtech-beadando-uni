f60c2c0bc9aa933d99935cdbb76662cb88ff917e
bajuszmate <bajszmate99@gmail.com>
2019-11-10 18:00:23 +0100

    first commit

---
+++ student/js/main.js
@@ -0,0 +1,5 @@
+// create form to add cars
+//create form to add manufacturers
+//on index page, show manufacturers and their cars. manufacturers should be selected, and stored in cookies.
+function GetCars() {
+}
